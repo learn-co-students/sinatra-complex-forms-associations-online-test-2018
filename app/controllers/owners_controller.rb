9ecf3657b3221aee9959b89a333e3c8d83fdad85
mkdoh3 <mkdoherty3@gmail.com>
2019-08-21 11:24:49 -0500

    student examples

--- app/controllers/owners_controller.rb
+++ app/controllers/owners_controller.rb
@@ -1,29 +1,42 @@
 class OwnersController < ApplicationController
-
   get '/owners' do
     @owners = Owner.all
-    erb :'/owners/index' 
+    erb :'/owners/index'
   end
 
-  get '/owners/new' do 
+  get '/owners/new' do
+    @pets = Pet.all
     erb :'/owners/new'
   end
 
-  post '/owners' do 
-    
+  post '/owners' do
+    @owner = Owner.create(params['owner'])
+    if !params[:pet][:name].empty?
+      @owner.pets << Pet.create(name: params[:pet][:name])
+    end
+    redirect "/owners/#{@owner.id}"
   end
 
-  get '/owners/:id/edit' do 
+  get '/owners/:id/edit' do
+    @pets = Pet.all
     @owner = Owner.find(params[:id])
     erb :'/owners/edit'
   end
 
-  get '/owners/:id' do 
+  get '/owners/:id' do
     @owner = Owner.find(params[:id])
     erb :'/owners/show'
   end
 
-  patch '/owners/:id' do 
-   
+  patch '/owners/:id' do
+    #if no boxes were checked, add the pet_id key to the owner hash and set it = []
+    # active record will then properly update the association, removing any previous pets
+    params[:owner]['pet_ids'] = [] if !params[:owner].keys.include?('pet_ids')
+    @owner = Owner.find(params[:id])
+    @owner.update(params['owner'])
+    if !params['pet']['name'].empty?
+      @owner.pets << Pet.create(name: params['pet']['name'])
+    end
+    redirect "owners/#{@owner.id}"
   end
 end
--- spec/controllers/owners_controller_spec.rb
+++ spec/controllers/owners_controller_spec.rb
@@ -1,8 +1,7 @@
 require 'spec_helper'
 
-describe "Owners Controller" do
-  describe "new action" do 
-
+describe 'Owners Controller' do
+  describe 'new action' do
     it "can visit '/owners/new'" do
       get '/owners/new'
       expect(last_response.status).to eq(200)
@@ -14,8 +13,8 @@ describe "Owners Controller" do
     end
 
     it "'/owners/new' form has a checkbox for existing pets" do
-      @pet1 = Pet.create(:name => "Bessie")
-      @pet2 = Pet.create(:name => "Sadie")
+      @pet1 = Pet.create(name: 'Bessie')
+      @pet2 = Pet.create(name: 'Sadie')
       visit '/owners/new'
       expect(page.has_unchecked_field?(@pet1.id)).to eq(true)
       expect(page.has_unchecked_field?(@pet2.id)).to eq(true)
@@ -26,51 +25,51 @@ describe "Owners Controller" do
       expect(page).to have_field('pet[name]')
     end
 
-
     it "'/owners/new' creates a new owner and associates an existing pet " do
-      @pet1 = Pet.create(:name => "Bessie") 
-      @pet2 = Pet.create(:name => "Sadie")
+      @pet1 = Pet.create(name: 'Bessie')
+      @pet2 = Pet.create(name: 'Sadie')
       visit '/owners/new'
-      fill_in "owner[name]", :with => "Sophie"
+      fill_in 'owner[name]', with: 'Sophie'
       check(@pet1.id)
-      click_button "Create Owner"
+      click_button 'Create Owner'
       @owner = Owner.last
-      expect(@owner.name).to eq("Sophie")
-      expect(@owner.pets.first.name).to eq("Bessie")
+      # binding.pry
+      expect(@owner.name).to eq('Sophie')
+      expect(@owner.pets.first.name).to eq('Bessie')
     end
 
-      it "'/owners/new' creates a new owner and a new pet" do
-      @pet1 = Pet.create(:name => "Bessie") 
-      @pet2 = Pet.create(:name => "Sadie")
+    it "'/owners/new' creates a new owner and a new pet" do
+      @pet1 = Pet.create(name: 'Bessie')
+      @pet2 = Pet.create(name: 'Sadie')
       visit '/owners/new'
-      fill_in "owner[name]", :with => "Sophie"
-      fill_in "pet[name]", :with => "Caldwell"
-      click_button "Create Owner"
+      fill_in 'owner[name]', with: 'Sophie'
+      fill_in 'pet[name]', with: 'Caldwell'
+      click_button 'Create Owner'
       @owner = Owner.last
       @pet = Pet.last
-      expect(@owner.name).to eq("Sophie")
-      expect(@owner.pets.first.name).to eq("Caldwell")
+      expect(@owner.name).to eq('Sophie')
+      expect(@owner.pets.first.name).to eq('Caldwell')
     end
 
     it "'/owners/new' redirects to '/owners/:id' after form submissions" do
-      @pet1 = Pet.create(:name => "Bessie") 
-      @pet2 = Pet.create(:name => "Sadie")
+      @pet1 = Pet.create(name: 'Bessie')
+      @pet2 = Pet.create(name: 'Sadie')
       visit '/owners/new'
-      fill_in "owner[name]", :with => "Sophie"
+      fill_in 'owner[name]', with: 'Sophie'
       check(@pet1.id)
-      click_button "Create Owner"
+      click_button 'Create Owner'
       @owner = Owner.last
       expect(page.current_path).to eq("/owners/#{@owner.id}")
     end
   end
 
-  describe "edit action" do 
+  describe 'edit action' do
     before(:each) do
-      @owner = Owner.create(:name => "Carla")
-      @pet = Pet.create(:name => "Chewie", :owner_id => @owner.id)
+      @owner = Owner.create(name: 'Carla')
+      @pet = Pet.create(name: 'Chewie', owner_id: @owner.id)
     end
 
-    it "can visit '/owners/:id/edit' " do 
+    it "can visit '/owners/:id/edit' " do
       get "/owners/#{@owner.id}/edit"
       expect(last_response.status).to eq(200)
     end
@@ -82,29 +81,26 @@ describe "Owners Controller" do
       expect(page).to have_field('pet[name]')
     end
 
-     it "edit's the owner's name" do
+    it "edit's the owner's name" do
       visit "/owners/#{@owner.id}/edit"
-      fill_in "owner[name]", :with => "Carla Gremillion"
-      click_button "Update Owner"
-      expect(Owner.last.name).to eq("Carla Gremillion")
+      fill_in 'owner[name]', with: 'Carla Gremillion'
+      click_button 'Update Owner'
+      expect(Owner.last.name).to eq('Carla Gremillion')
     end
 
     it "edit's the owner's pets with an existing pet" do
-      @shaggy = Pet.create(:name => "Shaggy")
+      @shaggy = Pet.create(name: 'Shaggy')
       visit "/owners/#{@owner.id}/edit"
       check(@shaggy.id)
-      click_button "Update Owner"
-      expect(Owner.last.pets.last.name).to eq("Shaggy")
+      click_button 'Update Owner'
+      expect(Owner.last.pets.last.name).to eq('Shaggy')
     end
 
     it "edit's the owner's pets with a new pet" do
       visit "/owners/#{@owner.id}/edit"
-      fill_in "pet[name]", :with => "Carlton"
-      click_button "Update Owner"
-      expect(Owner.last.pets.last.name).to eq("Carlton")
+      fill_in 'pet[name]', with: 'Carlton'
+      click_button 'Update Owner'
+      expect(Owner.last.pets.last.name).to eq('Carlton')
     end
-
-
   end
-
 end
